3b5ac0bedea347fe1d8fc8b87b0cfd5dc7d8a0e4
Leandro Saracco <leosaracco@gmail.com>
2022-05-24 14:26:55 -0300

    Add files via upload

--- src/main/java/ar/com/cdt/questionados/repository/IPreguntasRepository.java
+++ src/main/java/ar/com/cdt/questionados/repository/IPreguntasRepository.java
@@ -8,8 +8,4 @@ import ar.com.cdt.questionados.models.Preguntas;
 @Repository
 public interface IPreguntasRepository extends JpaRepository<Preguntas, Integer> {
 
-//	//FIXME
-//	@Query("SELECT * FROM preguntas p INNER JOIN respuesta r ON p.id_pregunta=r.pregunta_id WHERE p.id_pregunta=:id_pregunta")
-//	public ArrayList<Preguntas> FindByIdComplete(@Param("id_pregunta")int id);
-	
 }
